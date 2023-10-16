78e4e435fbf4e5522f9766f596b9a9ba5014e596
Natalya <natalyasafarevich@gmail.com>
2023-10-16 21:37:19 +0200

    project is updated

---
+++ app/news/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { FC } from "react";
+
+async function getData() {
+	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	return response.json();
+}
+
+async function News() {
+	const post = await getData();
+	console.log(post);
+	return (
+		<div className="news">
+			<h1 className="news__title">News Title</h1>
+			<ul>
+				{post.map((posts: any, i: number) => {
+					return (
+						<li key={i}>
+							<Link href={`/news/${posts.id}`}>{posts.title}</Link>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+export default News;
---
+++ componets/Navigation.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavLinkprops = {
+	label: string;
+	href: string;
+};
+type Props = {
+	navLink: Array<NavLinkprops>;
+};
+export default function Navigation({ navLink }: Props) {
+	const pathname = usePathname();
+	console.log(pathname);
+
+	return (
+		<>
+			{navLink.map((link) => {
+				const isActive = pathname === link.href;
+				return (
+					<Link href={link.href} className={isActive ? "active" : ""}>
+						{link.label}
+					</Link>
+				);
+			})}
+		</>
+	);
+}
