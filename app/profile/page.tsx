d88f98ceaf982ec4a4409e50d50bbc1742f22294
Natalya <natalyasafarevich@gmail.com>
2023-10-23 15:00:35 +0200

    Image is added

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,8 @@
 import { authConfig } from "@/configs/auth";
 import { getServerSession } from "next-auth";
+import Image from 'next/image';
+import Img from  './man-avatar.jpg'
+
 
 export default async function Profile() {
   const session = await getServerSession(authConfig); // Передайте `req`, если он не является частью `authConfig`
@@ -7,7 +10,15 @@ export default async function Profile() {
   return (
     <div>
       <h1>Profile of {session?.user?.name}</h1>
-      {session?.user?.image && <img src={session.user.image} alt="" />}
+      <Image
+       src={Img}
+       width={Img.width}
+       height={Img.height}
+       style={{objectFit:'contain'}}
+       sizes="100vw"
+       alt="Picture of the author"
+      />
+      {/* {session?.user?.image && <img src={session.user.image} alt="" />} */}
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { // c сайта
+  images: {
+    formats: [
+      'image/webp', 'image/avif'
+    ],
+    deviceSizes: [
+      375,
+      475,
+      575,
+      991,
+      1200
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
